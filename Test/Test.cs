fce7bfcecb95fcfa626fa34fe3936071c7e8f2d8
kotorkovsciy <kotorkovsciy@gmail.com>
2025-01-11 17:09:17 +0300

    test: add cases with different numbers of intervals for intersection testing

--- Test/Test.cs
+++ Test/Test.cs
@@ -95,4 +95,55 @@ public class Test
 
         Assert.Equal(expected, solution.Intersection(user1, user2));
     }
+
+    [Fact]
+    public void TestDifferentNumberOfIntervals()
+    {
+        var solution = new Solution();
+
+        var user1 = new List<List<int>> {
+                new List<int> { 1, 4 },
+                new List<int> { 5, 8 },
+                new List<int> { 10, 12 },
+                new List<int> { 14, 18 }
+            };
+        var user2 = new List<List<int>> { new List<int> { 3, 6 } };
+        var expected = new List<List<int>> { new List<int> { 3, 4 }, new List<int> { 5, 6 } };
+
+        Assert.Equal(expected, solution.Intersection(user1, user2));
+
+        user1 = new List<List<int>> { new List<int> { 2, 6 } };
+        user2 = new List<List<int>> {
+                new List<int> { 1, 3 },
+                new List<int> { 4, 5 },
+                new List<int> { 7, 10 }
+            };
+        expected = new List<List<int>> { new List<int> { 2, 3 }, new List<int> { 4, 5 } };
+
+        Assert.Equal(expected, solution.Intersection(user1, user2));
+
+        user1 = new List<List<int>> { new List<int> { 0, 20 } };
+        user2 = new List<List<int>> {
+                new List<int> { 5, 10 },
+                new List<int> { 12, 15 },
+                new List<int> { 18, 25 }
+            };
+        expected = new List<List<int>> {
+                new List<int> { 5, 10 },
+                new List<int> { 12, 15 },
+                new List<int> { 18, 20 }
+            };
+
+        Assert.Equal(expected, solution.Intersection(user1, user2));
+
+        user1 = new List<List<int>> {
+                new List<int> { 1, 3 },
+                new List<int> { 5, 7 },
+                new List<int> { 9, 12 }
+            };
+        user2 = new List<List<int>> { new List<int> { 6, 10 } };
+        expected = new List<List<int>> { new List<int> { 6, 7 }, new List<int> { 9, 10 } };
+
+        Assert.Equal(expected, solution.Intersection(user1, user2));
+    }
 }
