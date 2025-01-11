4b8f0f6428ce9b1cbd90fc16a8344c18472d91df
Alias <heh@kek.com>
2025-01-11 21:39:12 +0800

    all test is completed

--- Intersection/Solution.cs
+++ Intersection/Solution.cs
@@ -1,10 +1,90 @@
-﻿namespace Intersection
+﻿namespace Intersection;
+
+public class Solution
 {
-    public class Solution
+    public List<List<int>> Intersection(List<List<int>> user1, List<List<int>> user2)
     {
-        public List<List<int>> Intersection(List<List<int>> user1, List<List<int>> user2)
+        List<List<int>> Answer = new();
+        var iterA = 0;
+        var iterB = 0;
+
+        if (user1.Count == 0 || user2.Count == 0) return Answer;
+        while (iterA < user1.Count || iterB < user2.Count)
         {
-            
+            if (user1[iterA][0] > user2[iterB][1])
+            {
+                if (iterB < user2.Count - 1)
+                {
+                    iterB++;
+                    continue;
+                }
+
+                break;
+            }
+
+            if (user2[iterB][0] > user1[iterA][1])
+            {
+                if (iterA < user1.Count - 1)
+                {
+                    iterA++;
+                    continue;
+                }
+
+                break;
+            }
+
+            List<int> inner = new();
+            List<int> outer = new();
+
+            List<int> answer = new();
+            var isainner = false;
+            if (user1[iterA][0] <= user2[iterB][1] && user1[iterA][0] >= user2[iterB][0])
+            {
+                inner = user1[iterA];
+                outer = user2[iterB];
+                isainner = true;
+            }
+            else
+            {
+                inner = user2[iterB];
+                outer = user1[iterA];
+            }
+
+            answer.Add(inner[0]);
+            if (inner[1] <= outer[1])
+                answer.Add(inner[1]);
+            else
+                answer.Add(outer[1]);
+
+            Answer.Add(answer);
+
+            if (isainner && iterA != user1.Count - 1)
+            {
+                iterA++;
+                continue;
+            }
+
+            if (!isainner && iterB != user2.Count - 1)
+            {
+                iterB++;
+                continue;
+            }
+
+            if (iterA != user1.Count - 1)
+            {
+                iterA++;
+                continue;
+            }
+
+            if (iterB != user2.Count - 1)
+            {
+                iterB++;
+                continue;
+            }
+
+            break;
         }
+
+        return Answer;
     }
 }
--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 ﻿# Interval Intersections 📏
-### Description
-Given two sorted lists of intervals, implement a function to find their intersections. An interval [a, b] intersects with another interval [c, d] if there exists a number x such that x lies in both intervals. The intersection of [a, b] and [c, d] is the interval [max(a, c), min(b, d)], provided max(a, c) <= min(b, d).
 
+### Description
+
+Given two sorted lists of intervals, implement a function to find their intersections. An interval [a, b] intersects
+with another interval [c, d] if there exists a number x such that x lies in both intervals. The intersection of [a, b]
+and [c, d] is the interval [max(a, c), min(b, d)], provided max(a, c) <= min(b, d).
 
 #### Input:
+
 user1 and user2: Lists of intervals, where each interval is represented as [start, end] (both inclusive).
 Each list is sorted by the start of the intervals.
 Intervals within a list do not overlap.
+
 #### Output:
+
 A list of intervals representing the intersections of user1 and user2.
